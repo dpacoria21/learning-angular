ddb39338b92d0a63feb3b1a328d1630d04a2d1d0
Diego Pacori <dpacoria@unsa.edu.pe>
2023-04-24 18:09:15 -0500

    Se creo nuestro primero modulo para ver como es el tipado en TS

--- typescript-intro/src/main.ts
+++ typescript-intro/src/main.ts
@@ -1,5 +1,5 @@
 import './style.css'
-
+import './topics/01-basic-typescript'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   Hola Mundo!!
---
+++ typescript-intro/src/topics/01-basic-typescript.ts
@@ -0,0 +1,12 @@
+
+
+(() => {
+    const name: string = 'Diego';
+
+    let hpPoints: number | string = 25;
+    hpPoints = 'FULL';
+
+    const isAlive: boolean = true;
+
+    console.log({name, hpPoints, isAlive});
+})();
