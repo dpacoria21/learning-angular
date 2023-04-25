8d02bb0d2d7e8102e8c5281bc25a09199bf45b02
Diego Pacori <dpacoria@unsa.edu.pe>
2023-04-24 20:58:43 -0500

    Se trabajo con las clases en TS, herencia, y formas rapidas de inicializar valores en el constructor y como inyectar otras clases a otra sin necesidad de usar herencia

---
+++ typescript-intro/src/topics/08-classes.ts
@@ -0,0 +1,46 @@
+
+
+export class Person {
+
+    // public name: string;
+    // private address: string;
+
+    constructor(
+        public name: string,
+        public lastName: string, 
+        private address?: string
+    ){}
+
+}
+
+
+// export class Hero extends Person {
+
+
+
+//     constructor(
+//         public afterEgo: string,
+//         public age: number,
+//         public realName: string,
+//     ) {
+//         super(realName, 'New York');
+//     }
+    
+// }
+export class Hero{
+
+    constructor(
+        public afterEgo: string,
+        public age: number,
+        public realName: string,
+        public person: Person,
+    ) {
+    }
+    
+}
+
+const person = new Person('Tony','Stark','New York');
+
+const ironman = new Hero('Ironman', 45 ,'Tony', person);
+
+console.log(ironman);
