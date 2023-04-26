d530f87a5adb4b054c0380ac487b7a99750b9c4d
Diego Pacori <dpacoria@unsa.edu.pe>
2023-04-25 20:49:38 -0500

    Se separo la funcionalidad del Contador en un componente

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -8,18 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   public title: string = 'Hola Mundo';
-  public counter: number = 10;
-
-  increaseBy(value: number = 1): void {
-    this.counter += value;
-  }
-
-  reset(): void {
-    this.counter = 10;
-  }
-
-  // degreeBy(): void {
-  //   this.counter--;
-  // }
 
 }
--- 02-bases/src/app/counter/counter.component.ts
+++ 02-bases/src/app/counter/counter.component.ts
@@ -4,8 +4,24 @@ import {Component} from '@angular/core';
     selector: 'app-counter',
     template: `
         <h1>Hola desde CounterComponent</h1>
+        <h3>
+            Counter: {{counter}}
+        </h3>
+        <button (click)="increaseBy(-1)">-1</button>
+        <button (click)="reset()">Reset</button>
+        <button (click)="increaseBy()">+1</button>
     `
 })
 export class CounterComponent {
+
+    public counter: number = 10;
+    
+    increaseBy(value: number = 1): void {
+        this.counter += value;
+    }
+
+    reset(): void {
+        this.counter = 10;
+    }
 
 }
