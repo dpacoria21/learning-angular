3312c13749592e8f90433acfd7e9bc5713ae62ca
Diego Pacori <dpacoria@unsa.edu.pe>
2023-04-25 20:38:55 -0500

    Creando nuestro propio componente

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule
