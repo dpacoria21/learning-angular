f8ffa86fdd83863f800d773fd41fcb84ed743a7b
Diego Pacori <dpacoria@unsa.edu.pe>
2023-04-25 21:29:47 -0500

    Se realizo la tarea de cambiar nombre y edad

--- 02-bases/src/app/heroes/hero/hero.component.ts
+++ 02-bases/src/app/heroes/hero/hero.component.ts
@@ -18,4 +18,12 @@ export class HeroComponent {
     return `${this.name} - ${this.age}`;
   }
 
+  changeHero(): void {
+    this.name = 'superman';
+  }
+
+  changeAge(): void {
+    this.age = 23;
+  }
+
 }
