0a7cdac54ac4deef29e6f023a950d5b1051a6fcd
Diego Pacori <dpacoria@unsa.edu.pe>
2023-04-25 20:30:52 -0500

    Se realizo un contador en Angular

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -6,8 +6,20 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  public title: string = 'Hola Mundo';
 
+  public title: string = 'Hola Mundo';
   public counter: number = 10;
+
+  increaseBy(value: number = 1): void {
+    this.counter += value;
+  }
+
+  reset(): void {
+    this.counter = 10;
+  }
+
+  // degreeBy(): void {
+  //   this.counter--;
+  // }
 
 }
