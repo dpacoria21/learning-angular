6afdb3f49af3daef4541d79331ef1cdbe3bd2062
Diego Pacori <dpacoria@unsa.edu.pe>
2023-04-28 23:48:03 -0500

    Se convirtio el servicio a privado como buena practica en Angular

--- 02-bases/src/app/dbz/pages/main-page.component.ts
+++ 02-bases/src/app/dbz/pages/main-page.component.ts
@@ -10,8 +10,20 @@ import { DbzService} from "../services/dbz.service";
 })
 export class MainPageComponent {
 
-    constructor( public dbzService: DbzService ) {
+    constructor( private dbzService: DbzService ) {
         
+    }
+
+    get getCharacters(): Character[] {
+        return [...this.dbzService.characters];
+    }
+
+    onDeleteCharacter(id: string): void {
+        this.dbzService.deleteCharacterById(id);
+    }
+
+    onNewCharacter(character: Character): void {
+        this.dbzService.recibirCharacter(character);
     }
 
 }
